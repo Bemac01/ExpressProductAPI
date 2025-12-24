a3dcdbd8014bc35c7464f03891868b6a7186dae6
bemac01 <nwankwochidubem01@gmail.com>
2025-12-24 23:17:25 +0100

    file Updated

---
+++ models/productsModel.js
@@ -0,0 +1,17 @@
+let products = [
+    {
+        id: 1,
+        product: 'Made in Nigeria shoe',
+        price : 34930,
+        instock: 20
+    },
+    {
+        id: 2,
+        product: 'LuisVitton T-shirt',
+        price : 20000,
+        instock: 25
+    }
+
+] 
+
+module.exports = products;
--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,30 @@
 const express = require('express');
+const products = require('../models/productsModel');
 
 const router = express.Router();
 
-
+//get all products 
 router.get('/', (req, res) => {
-    res.json({msg : 'all is working well'});
-})
+    res
+    .status(200)
+    .json(products);
+});
+
+
+//get a product
+router.get('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const product = products.find(item => item.id === id);
+
+    if(!product){
+      return res
+        .status(404)
+        .json({msg : `Product not found`});
+    }
+    
+    res.status(200).json(product)
+
+});
 
 
 module.exports = router;
