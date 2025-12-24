4843132bf26b698ca63c48ed83c69cc7ed707951
bemac01 <nwankwochidubem01@gmail.com>
2025-12-25 00:49:35 +0100

    file Updated

--- routes/routes.js
+++ routes/routes.js
@@ -21,10 +21,41 @@ router.get('/:id', (req, res) => {
         .status(404)
         .json({msg : `Product not found`});
     }
-    
+
     res.status(200).json(product)
 
 });
 
+
+//add new product
+router.post('/add', (req, res) => {
+    const newProducts = {
+        id: products.length + 1,
+        ...req.body
+    }
+
+    products.push(newProducts);
+    res.status(201).json({
+        msg: 'product added successfully',
+        products
+    });
+});
+
+//update a product 
+router.put('/update/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = products.findIndex(u => u.id === id);
+    
+    if (index === -1) {
+      return res
+        .status(404)
+        .json({ error: 'User not found' });
+    }
+    
+    products[index] = { id, ...req.body };
+    res.json(products[index]);
+  });
+
+  
 
 module.exports = router;
